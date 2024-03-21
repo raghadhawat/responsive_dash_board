f0b92d73df6ec88519e85a650abd13e7acf2156d
raghadhawat <hawatraghad123@gmail.com>
2024-03-21 16:35:43 +0300

    transaction history part 2

--- lib/widgets/transaction_history.dart
+++ lib/widgets/transaction_history.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:rsponsive_dashbaord/utlis/app_styles.dart';
+import 'package:rsponsive_dashbaord/widgets/transaction_history_header.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              'Transaction History',
-              style: AppStyles.styleSemiBold20,
-            ),
-            Text(
-              'See all',
-              style: AppStyles.styleMedium16
-                  .copyWith(color: const Color(0xff4EB7F2)),
-            )
-          ],
+        TransactionHistoryHeader(),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          '13 April 2022',
+          style: AppStyles.styleMedium16,
         )
       ],
     );
