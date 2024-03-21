729d42b79b19893db6a5ea2ac77795132779ae16
raghadhawat <hawatraghad123@gmail.com>
2024-03-21 16:43:46 +0300

    transaction history part 3

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:rsponsive_dashbaord/models/transaction_model.dart';
+import 'package:rsponsive_dashbaord/utlis/app_styles.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({super.key, required this.transactionModel});
+  final TransactionModel transactionModel;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      color: const Color(0xffFAFAFA),
+      child: ListTile(
+        title: Text(
+          transactionModel.title,
+          style: AppStyles.styleSemiBold16,
+        ),
+        subtitle: Text(
+          transactionModel.date,
+          style: AppStyles.styleRegular16.copyWith(
+            color: Color(0xffAAAAAA),
+          ),
+        ),
+        trailing: Text(
+          transactionModel.amount,
+          style: AppStyles.styleSemiBold20.copyWith(
+            color: transactionModel.isWithdrawal
+                ? const Color(0xffF3735E)
+                : const Color(0xff7DD97B),
+          ),
+        ),
+      ),
+    );
+  }
+}
