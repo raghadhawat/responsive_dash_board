c1792dd9bafd605d2dc72aea10575de6f5a66c04
raghadhawat <hawatraghad123@gmail.com>
2024-03-26 12:41:21 +0300

    resoinsive mobile layout part three

--- lib/widgets/all_expensess_item_header.dart
+++ lib/widgets/all_expensess_item_header.dart
@@ -12,18 +12,23 @@ class AllExpensessItemHeader extends StatelessWidget {
     return Row(
       children: [
         Flexible(
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              decoration: ShapeDecoration(
-                color: imageBackgrund ?? const Color(0xFFFAFAFA),
-                shape: const OvalBorder(),
-              ),
-              child: Center(
-                child: SvgPicture.asset(
-                  image,
-                  colorFilter: ColorFilter.mode(
-                      imageColor ?? const Color(0xFF4EB7F2), BlendMode.srcIn),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 60,
+            ),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                decoration: ShapeDecoration(
+                  color: imageBackgrund ?? const Color(0xFFFAFAFA),
+                  shape: const OvalBorder(),
+                ),
+                child: Center(
+                  child: SvgPicture.asset(
+                    image,
+                    colorFilter: ColorFilter.mode(
+                        imageColor ?? const Color(0xFF4EB7F2), BlendMode.srcIn),
+                  ),
                 ),
               ),
             ),
